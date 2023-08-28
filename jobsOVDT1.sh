f70fcdd54ae08ebf2c83441a760a63aff8d02fa7
Evandro Jardini <eajardini@yahoo.com.br>
2023-08-28 09:38:17 -0300

    Add files via upload

---
+++ jobsOVDT1.sh
@@ -0,0 +1,4 @@
+# caminho_do_kitchen/kitchen.sh -file: "caminho_do_job"
+
+sh /opt/install/pentaho/data-integration/kitchen.sh -file:"/home/eajardini/Dropbox/apostilas/posgrad/ifsp/scripts/capituloETL/bi_OVDT1.kjb"
+
